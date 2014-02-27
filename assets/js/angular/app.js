e52a1101ba64d9b2eb5f194f111a0e5be1b0baf8
Marc Alonzo <marc.j.alonzo@gmail.com>
2014-02-27 10:25:48 -0800

    Added angular-animate.min.js

--- assets/js/angular/app.js
+++ assets/js/angular/app.js
@@ -25,5 +25,37 @@ angular
 			.state('cs3', {
 				url: '/cs3',
 				templateUrl: 'templates/cs3.html'
-			})
+			})		
+
+		// $stateProvider
+		// 	.state('home', {
+		// 		url: '/',
+		// 		views: {
+		// 			viewMainContent: { templateUrl: 'templates/home.html' }
+		// 		}
+		// 	})
+		// 	.state('contact', {
+		// 		url: '/contact',
+		// 		views: {
+		// 			viewMainContent: { templateUrl: 'templates/contact.html' }
+		// 		}
+		// 	})
+		// 	.state('cs1', {
+		// 		url: '/cs1',
+		// 		views: {
+		// 			viewSlideInRight: { templateUrl: 'templates/cs1.html' }
+		// 		}
+		// 	})
+		// 	.state('cs2', {
+		// 		url: '/cs2',
+		// 		views: {
+		// 			viewSlideInRight: { templateUrl: 'templates/cs2.html' }
+		// 		}
+		// 	})
+		// 	.state('cs3', {
+		// 		url: '/cs3',
+		// 		views: {
+		// 			viewSlideInRight: { templateUrl: 'templates/cs3.html' }
+		// 		}
+		// 	})
 	}])
