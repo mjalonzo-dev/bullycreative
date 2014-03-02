3d0901dc84b1eaa31382509a7f99c3dce901758f
Marc Alonzo <marc.j.alonzo@gmail.com>
2014-03-02 12:23:48 -0800

    Added images and got angular working

--- assets/js/angular/app.js
+++ assets/js/angular/app.js
@@ -1,61 +1,50 @@
-angular
-	.module('app', [
-		'ui.router'
-	])
-	.config(['$urlRouterProvider','$stateProvider', function($urlRouterProvider, $stateProvider) {
-		$urlRouterProvider.otherwise('/');
-
-		$stateProvider
-			.state('home', {
-				url: '/',
-				templateUrl: 'templates/home.html'
-			})
-			.state('contact', {
-				url: '/contact',
-				templateUrl: 'templates/contact.html'
-			})
-			.state('cs1', {
-				url: '/cs1',
-				templateUrl: 'templates/cs1.html'
-			})
-			.state('cs2', {
-				url: '/cs2',
-				templateUrl: 'templates/cs2.html'
-			})
-			.state('cs3', {
-				url: '/cs3',
-				templateUrl: 'templates/cs3.html'
-			})		
-
-		// $stateProvider
-		// 	.state('home', {
-		// 		url: '/',
-		// 		views: {
-		// 			viewMainContent: { templateUrl: 'templates/home.html' }
-		// 		}
-		// 	})
-		// 	.state('contact', {
-		// 		url: '/contact',
-		// 		views: {
-		// 			viewMainContent: { templateUrl: 'templates/contact.html' }
-		// 		}
-		// 	})
-		// 	.state('cs1', {
-		// 		url: '/cs1',
-		// 		views: {
-		// 			viewSlideInRight: { templateUrl: 'templates/cs1.html' }
-		// 		}
-		// 	})
-		// 	.state('cs2', {
-		// 		url: '/cs2',
-		// 		views: {
-		// 			viewSlideInRight: { templateUrl: 'templates/cs2.html' }
-		// 		}
-		// 	})
-		// 	.state('cs3', {
-		// 		url: '/cs3',
-		// 		views: {
-		// 			viewSlideInRight: { templateUrl: 'templates/cs3.html' }
-		// 		}
-		// 	})
-	}])
+// Declare the main module
+var blyApp = angular.module('blyApp', [
+    'ngRoute',
+    'ngAnimate'
+]);
+// Initialize the main module
+blyApp.run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
+    'use strict';
+    /**
+     * Helper method for main page transitions. Useful for specifying a new page partial and an arbitrary transition.
+     * @param  {String} path               The root-relative url for the new route
+     * @param  {String} pageAnimationClass A classname defining the desired page transition
+     */
+    $rootScope.go = function (path, pageAnimationClass) {
+        if (typeof(pageAnimationClass) === 'undefined') { // Use a default, your choice
+            $rootScope.pageAnimationClass = 'crossFade';
+        }
+        
+        else { // Use the specified animation
+            $rootScope.pageAnimationClass = pageAnimationClass;
+        }
+        if (path === 'back') { // Allow a 'back' keyword to go to previous page
+            $window.history.back();
+        }
+        
+        else { // Go to the specified path
+            $location.path(path);
+        }
+    };
+}]);
+// Configure the main module
+blyApp.config(['$routeProvider', function ($routeProvider) {
+    'use strict';
+    $routeProvider
+        .when('/cs1', {
+            templateUrl: 'templates/cs1.html'
+        })
+        .when('/cs2', {
+            templateUrl: 'templates/cs2.html'
+        })
+        .when('/cs3', {
+            templateUrl: 'templates/cs3.html'
+        })
+        .when('/contact', {
+            templateUrl: 'templates/contact.html'
+        })
+        .otherwise({
+           templateUrl: 'templates/home.html' 
+        });
+}]);
